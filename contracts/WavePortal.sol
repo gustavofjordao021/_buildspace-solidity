a2eca2198f53b460d8d46481b714f1bef38037f0
Gustavo Jordão <gustavo.jordao021@gmail.com>
2021-09-10 09:19:20 -0400

    Adjustments to finalize project

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    // Declaration of state variables that are permanently stored in contract storage.
     uint totalWaves;
+    uint private seed;
 
     // Emits an event which can be handled client-side
     event NewWave(address indexed from, uint timestamp, string message);
+
+    // Declares a mapping to associate incoming messages to their sender's address
+    mapping(address => uint) public lastWavedAt;
 
     // Custom Data type that can be customized and holds data inside it
     struct Wave {
@@ -24,19 +29,36 @@ contract WavePortal {
         console.log("Yo yo, we have been re-constructed!");
     }
 
-    // Function to register waves sent from client into the contract
+    // Function to register waves sent from client into the contract. It awards ETH to a random user based on dificulty set below
     function wave(string memory _message) public {
+        // Ensures that messages can only be sent every 30 seconds by checking the current timestamp vs. the previously stored one
+        require (lastWavedAt[msg.sender] + 30 seconds < block.timestamp, "Wait 30 seconds");
+
+        // Updates the current wave timestamp stored
+        lastWavedAt[msg.sender] = block.timestamp;
+        
         totalWaves += 1;
         console.log(" waved w/ the following message: ", msg.sender, _message);
 
+        // Creates a wave and adds it to array of structs called Waves. Then, emitss event for the client to pick up
         waves.push(Wave(msg.sender, _message, block.timestamp));
-
         emit NewWave(msg.sender, block.timestamp, _message);
 
-        uint prizeAmount = 0.0001 ether;
-        require(prizeAmount <= address(this).balance, "Trying to withdraw more ETH than what the contract has.");
-        (bool success,) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw ETH from contract.");
+        // Generate pseudrandom number based on block information in the range of 0 to 100
+        uint randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %s", randomNumber);
+
+        // Assign the generated random number as the seed for the next wave to improve security
+        seed = randomNumber;
+
+        // Gives a 15% chance that the user wins the prize
+        if (randomNumber < 15) {
+            console.log("%s won!", msg.sender);
+            uint prizeAmount = 0.0001 ether;
+            require(prizeAmount <= address(this).balance, "Trying to withdraw more ETH than what the contract has.");
+            (bool success,) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw ETH from contract.");
+        }        
     }
 
     // Function to return all waves sent to the contract
